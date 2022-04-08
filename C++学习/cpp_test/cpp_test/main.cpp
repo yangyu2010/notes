836978bdf857d818003e9a1f3fd5562f58c7715f
Yu Yang <yangyu2010@aliyun.com>
2022-04-08 17:05:53 +0800

    堆空间的申请和释放

--- C++学习/cpp_test/cpp_test/main.cpp
+++ C++学习/cpp_test/cpp_test/main.cpp
@@ -98,7 +98,7 @@ void swapInt3(int &a, int &b) {
     b = tmp;
 }
 
-int main(int argc, const char * argv[]) {
+int main1(int argc, const char * argv[]) {
 
     int a = 10;
     int b = 20;
@@ -108,5 +108,54 @@ int main(int argc, const char * argv[]) {
     
     cout << "a=" << a << ", b=" << b << endl;
  
+    return 0;
+}
+
+
+// ----------------------------------------------面向对象----------------------------------------------
+// ----------------------------------------------面向对象----------------------------------------------
+// ----------------------------------------------面向对象----------------------------------------------
+// ----------------------------------------------面向对象----------------------------------------------
+// ----------------------------------------------面向对象----------------------------------------------
+
+
+struct Person {
+    int m_id;
+    int m_age;
+    int m_height;
+    
+    void run() {
+        
+    }
+};
+
+int main2(int argc, const char * argv[]) {
+
+    Person person;
+    person.m_id = 1;
+    person.m_age = 2;
+    person.m_height = 3;
+    
+    cout << "&person " << &person << endl;
+    cout << "&m_id " << &person.m_id << endl;
+    cout << "&m_age " << &person.m_age << endl;
+    cout << "&m_height " << &person.m_height << endl;
+
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+
+    Person person;
+    person.m_id = 1;
+    person.m_age = 2;
+    person.m_height = 3;
+    
+    cout << "&person " << &person << endl;
+    cout << "&m_id " << &person.m_id << endl;
+    cout << "&m_age " << &person.m_age << endl;
+    cout << "&m_height " << &person.m_height << endl;
+
+    
     return 0;
 }
