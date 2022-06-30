4838b79aa6a71597d6f40b744279c82ecfce5498
Yu Yang <yangyu2010@aliyun.com>
2022-06-30 19:50:04 +0800

    递归-斐波那契数列

--- 算法2学习/21-递归/main.c
+++ 算法2学习/21-递归/main.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,20 +27,14 @@ int fib1(int index, int *array) {
     return array[index];
 }
 
-int fib2(int index, int *array) {
-    if (array[index] == 0) {
-        array[index] = fib1(index - 1, array) + fib1(index - 2, array);
-    }
-    return array[index];
-}
-
 void test1() {
     int s = sum(100);
     printf("%d", s);
 }
 
 void test2() {
-    int f = fib(40);
+    int size = 30;
+    int f = fib(size - 1);
     printf("%d", f);
 }
 
@@ -48,10 +43,77 @@ void test3() {
     int *array = (int *)calloc(size, sizeof(int));
     array[0] = 1;
     array[1] = 1;
-    int f = fib1(size, array);
+    int f = fib1(size - 1, array);
     printf("%d", f);
 }
 
+void test4() {
+    int size = 30;
+    int *array = (int *)calloc(size, sizeof(int));
+    array[0] = 1;
+    array[1] = 1;
+    for (int i = 2; i < size; i++) {
+        array[i] = array[i - 1] + array[i - 2];
+    }
+
+    printf("\n%d", array[size - 1]);
+}
+
+/*
+i & 1 == i % 2
+
+0 % 2 = 0
+1 % 2 = 1
+2 % 2 = 0
+3 % 2 = 1
+4 % 2 = 0
+5 % 2 = 1
+
+0 0b0000
+1 0b0001
+2 0b0010
+3 0b0011
+4 0b0100
+5 0b0101
+
+*/
+void test5() {
+    int size = 30;
+    int *array = (int *)calloc(2, sizeof(int));
+    array[0] = 1;
+    array[1] = 1;
+    for (int i = 2; i < size; i++) {
+        array[i & 1] = array[(i - 1) & 1] + array[(i - 2) & 1];
+    }
+
+    printf("\n%d", array[(size - 1) & 1]);
+}
+
+void test6() {
+    int size = 30;
+    int first = 1;
+    int second = 1;
+
+    // 2 3
+    // 5 8
+
+    for (int i = 2; i <= (size >> 1); i++) {
+        first = first + second;
+        second = first + second;
+    }
+
+    printf("%d\n", first);
+    printf("%d\n", second);
+}
+
+void test7() {
+    int size = 30;
+
+    double c = sqrt(5);
+    int fib = (pow((1 + c) / 2, size) - pow((1 - c) / 2, size)) / c;
+
+    printf("%d\n", fib);
+}
 
 int main() {
 
@@ -59,7 +121,7 @@ int main() {
     c_start = clock();    //!< 单位为ms
     t_start = time(NULL); //!< 单位为s
 
-    test3();
+    test7();
 
     c_end = clock();
     t_end = time(NULL);
