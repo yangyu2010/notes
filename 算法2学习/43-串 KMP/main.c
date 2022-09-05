cd87af29c506ffffa99ac9fd607e00a58a0920c2
Yu Yang <yangyu2010@aliyun.com>
2022-09-05 14:22:59 +0800

    添加KMP算法笔记

--- 算法2学习/43-串 KMP/main.c
+++ 算法2学习/43-串 KMP/main.c
@@ -3,6 +3,114 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/**
+Next数组
+aabaabaaa
+
+a a b a a b a a a
+0 1 2 3 4 5 6 7 8
+-----------------
+0 1 0 1 2 3 4 5 2
+
+1.  默认0的位置
+
+2.  i = 1 j = 0
+    a == a
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j
+
+3.  i = 2 j = 1
+    b != a
+    不相等时 j = next[j - 1] = 0;
+    然后再比较
+    还不相等
+    next[i] = j = 0;
+
+4.  i = 3 j = 0(上一步骤时把j==0了)
+    a == a
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j = 1
+
+5.  i = 4 j = 1
+    a == a
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j = 2
+
+6.  i = 5 j = 2
+    b == b
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j = 3
+
+7.  i = 6 j = 3
+    a == a
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j = 4
+
+8.  i = 7 j = 4
+    a == a
+    相等时 next[i] = next[i - 1] + 1 (next[i - 1]也就是现在j的值)
+    j++
+    next[i] = j = 5
+
+9.  i = 8 j = 5
+    a != b
+    不相等时 j = next[j - 1] = next[4] = 2;
+    然后再比较 
+    此时 pattern[j] = pattern[2] = b
+    pattern[i] = pattern[8] = a
+    还不相等
+    j = next[j - 1] = next[1] = 1;
+    此时 pattern[j] = pattern[1] = a
+    pattern[i] = pattern[8] = a
+    相等了
+    next[i] = j + 1 = 2;
+*/
+
+/**
+text:    abxabcabcaby
+pattern: abcaby
+
+next数组: [0, 0, 0, 1, 2, 0]
+abcaby
+012345
+------
+000120
+
+ti: text的索引下标
+pi: pattern的索引下标
+
+if (ti == pi) {
+    ti++;
+    pi++;
+} else {
+    pi = next[pi - 1]
+    然后再次比较
+    如果pi==0了 ti++
+}
+
+1. ti == 0, pi == 0 相等 
+2. ti == 1, pi == 1 相等
+3. ti == 2, pi == 2 
+    不相等
+    pi = next[pi - 1] = 0
+    再次比较 ti == 2, pi == 0 不相等
+4. ti == 3, pi == 0 相等
+5. ti == 4, pi == 1 相等
+6. ti == 5, pi == 2 相等
+...
+7. ti == 8, pi == 5 不相等
+    pi = next[pi - 1] = next[4] = 2
+    再次比较 ti == 8, pi == 2 相等
+8. ti == 9, pi == 3 相等
+...
+ */
+
 int indexOf(const char *, const char *);
 int *prefixSuffixArray(const char *);
 
@@ -29,8 +137,6 @@ int main() {
     assert(indexOf("abxabcabcaby", "abcaby") == 6);
     assert(indexOf("abxabcabcaby", "abcab") == 3);
     assert(indexOf("abxabcabcaby", "abcabc") == 3);
-
-
 
     const char *text = "abxabcabcaby";
     const char *pattern = "abcaby";
